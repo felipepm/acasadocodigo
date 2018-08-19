b72aaf1c637e1019c03a5bab9d95f53c15e8a125
FelipePM <1786655+felipepm@users.noreply.github.com>
2018-08-19 19:29:37 -0300

    Resources Config

--- src/main/java/br/com/alura/acasadocodigo/conf/AppWebConfiguration.java
+++ src/main/java/br/com/alura/acasadocodigo/conf/AppWebConfiguration.java
@@ -11,7 +11,9 @@ import org.springframework.format.support.FormattingConversionService;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import br.com.alura.acasadocodigo.controllers.HomeController;
@@ -24,7 +26,7 @@ import br.com.alura.acasadocodigo.models.CarrinhoCompras;
 									ProdutoDAO.class, 
 									FileSaver.class, 
 									CarrinhoCompras.class})
-public class AppWebConfiguration {
+public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 	
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver() {
@@ -62,5 +64,10 @@ public class AppWebConfiguration {
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){
+        configurer.enable();
+    }
 }
 	
